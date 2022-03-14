cd875e5ce24be589718d15e17a2deeae24754c2b
Sourcery AI <>
2022-03-14 08:42:28 +0000

    'Refactored by Sourcery'

--- pb_py/main.py
+++ pb_py/main.py
@@ -19,8 +19,8 @@ class Pandorabots:
         self.botname = botname
 
     def create_bot(self, botname):
-        path = '/bot/' + self.app_id +'/' + self.botname
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
         response = requests.put(url, params=query)
         if response.ok :
@@ -28,118 +28,110 @@ class Pandorabots:
         return response
     
     def list_bots(self):
-        path = '/bot/' + self.app_id
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.get(url, params=query)
-        return response
+        return requests.get(url, params=query)
 
     def delete_bot(self, botname):
-        path = '/bot/' + self.app_id +'/' + self.botname
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.delete(url, params=query)
-        return response
+        return requests.delete(url, params=query)
 
     def upload_file(self, filename):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id + '/' + self.botname + '/'
+        path = f'/bot/{self.app_id}/{self.botname}/'
         filepath = filename
         if '/' in filename:
             filename = filename.split('/')[-1]
         file_kind = filename.split('.')[-1]
-        if file_kind == 'pdefaults' or file_kind =='properties':
+        if file_kind in ['pdefaults', 'properties']:
             path += file_kind
-        if file_kind == 'map' or file_kind == 'set' or file_kind == 'substitution':
-            path += file_kind + '/' + filename.split('.')[0]
+        if file_kind in ['map', 'set', 'substitution']:
+            path += f'{file_kind}/' + filename.split('.')[0]
         if file_kind == 'aiml':
-            path += 'file/' + filename
-        if path == '/bot/' + self.app_id +'/' + self.botname + '/':
-            output = 'File type must be one of the following: substitution, properties, aiml, map, set, or pdefaults'
-            return output
-        url = "https://" + self.host + path
+            path += f'file/{filename}'
+        if path == f'/bot/{self.app_id}/{self.botname}/':
+            return 'File type must be one of the following: substitution, properties, aiml, map, set, or pdefaults'
+
+        url = f"https://{self.host}{path}"
         data = open(filepath,'rb').read()
         query = {"user_key": self.user_key}
-        response = requests.put(url, params=query, data=data)
-        return response
+        return requests.put(url, params=query, data=data)
 
     def list_files(self):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id + '/' + self.botname
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.get(url,params=query)
-        return response
+        return requests.get(url,params=query)
     
     def get_file(self, filename):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id + '/' + self.botname +'/'
+        path = f'/bot/{self.app_id}/{self.botname}/'
         file_kind = filename.split('.')[-1]
-        if file_kind == 'pdefaults' or file_kind =='properties':
+        if file_kind in ['pdefaults', 'properties']:
             path += file_kind
-        if file_kind == 'map' or file_kind == 'set' or file_kind == 'substitution':
-            path += file_kind + '/' + filename.split('.')[0]
+        if file_kind in ['map', 'set', 'substitution']:
+            path += f'{file_kind}/' + filename.split('.')[0]
         if file_kind == 'aiml':
-            path += 'file/' + filename
-        url = "https://" + self.host + path
+            path += f'file/{filename}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.get(url, params=query)
-        return response
+        return requests.get(url, params=query)
     
     def download_bot(self):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id + '/' + self.botname 
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key,
                  "return": 'zip'}
-        response = requests.get(url, params=query)
-        return response
+        return requests.get(url, params=query)
 
     def delete_file(self, filename):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id + '/' + self.botname + '/'
+        path = f'/bot/{self.app_id}/{self.botname}/'
         file_kind = filename.split('.')[-1]
-        if file_kind == 'pdefaults' or file_kind =='properties':
+        if file_kind in ['pdefaults', 'properties']:
             path += file_kind
-        if file_kind == 'map' or file_kind == 'set' or file_kind == 'substitution':
-            path += file_kind + '/' + filename.split('.')[0]
+        if file_kind in ['map', 'set', 'substitution']:
+            path += f'{file_kind}/' + filename.split('.')[0]
         if file_kind == 'aiml':
-            path += 'file/' + filename
-        if path == '/bot/' + self.app_id +'/' + self.botname + '/':
-            output = 'File type must be one of the following: substitution, properties, aiml, map, set, or pdefaults'
-            return output
-        url = "https://" + self.host + path
+            path += f'file/{filename}'
+        if path == f'/bot/{self.app_id}/{self.botname}/':
+            return 'File type must be one of the following: substitution, properties, aiml, map, set, or pdefaults'
+
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.delete(url, params=query)
-        return response
+        return requests.delete(url, params=query)
 
     def compile_bot(self):
         if not self.botname:
             return self.no_botname_error
-        path = '/bot/' + self.app_id +'/' + self.botname + '/verify'
-        url = "https://" + self.host + path
+        path = f'/bot/{self.app_id}/{self.botname}/verify'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
-        response = requests.get(url, params=query)
-        return response
+        return requests.get(url, params=query)
 
     def talk(self, input, usebotkey=False):
         if (usebotkey and not self.botkey) :
             return self.no_botkey_error
-        if (usebotkey and not self.host == 'api.pandorabots.com'):
+        if usebotkey and self.host != 'api.pandorabots.com':
             return self.use_api_host_error
         if not usebotkey and not self.botname:
             return self.no_botname_error
-        if not 'message' in input:
+        if 'message' not in input:
             return self.no_message_error
         path = '/atalk'
         if (not usebotkey):
-            path += '/' + self.app_id + '/' + self.botname
-        url = "https://" + self.host + path
+            path += f'/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"user_key": self.user_key}
         if usebotkey:
             query['botkey'] = self.botkey
@@ -154,7 +146,7 @@ class Pandorabots:
         if 'trace' in input:
             query['trace'] = str(input['trace'])
         if 'client_name' in input:
-            query['client_name'] = input['client_name'] 
+            query['client_name'] = input['client_name']
         if 'that' in input:
             query['that'] = input['that']
         if 'topic' in input:
@@ -163,22 +155,21 @@ class Pandorabots:
             query['reload'] = str(input['reload'])
         if 'extra' in input:
             query['extra'] = str(input['extra'])
-        response = requests.post(url, params=query)
-        return response
+        return requests.post(url, params=query)
     
     def atalk(self, input, usebotkey=False):
         if (usebotkey and not self.botkey) :
             return self.no_botkey_error
-        if (usebotkey and not self.host == 'api.pandorabots.com'):
+        if usebotkey and self.host != 'api.pandorabots.com':
             return self.use_api_host_error
         if not usebotkey and not self.botname:
             return self.no_botname_error
-        if not 'message' in input:
+        if 'message' not in input:
             return self.no_message_error
         path = '/atalk'
         if (not usebotkey):
-            path += '/' + self.app_id + '/' + self.botname
-        url = "https://" + self.host + path
+            path += f'/{self.app_id}/{self.botname}'
+        url = f"https://{self.host}{path}"
         query = {"input": input['message']}
         if usebotkey:
             query['botkey'] = self.botkey
@@ -189,6 +180,5 @@ class Pandorabots:
         if 'recent' in input: 
             query['recent'] = str(input['recent'])
         if 'client_name' in input:
-            query['client_name'] = input['client_name'] 
-        response = requests.post(url, params=query)
-        return response
+            query['client_name'] = input['client_name']
+        return requests.post(url, params=query)
